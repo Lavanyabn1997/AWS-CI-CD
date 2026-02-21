7804eea05f0ff9ba6e485ff18f275d33dc265928
Lavanyabn1997 <varshalavanya24@gmail.com>
2026-02-21 21:23:24 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,11 @@ echo "Logging into ECR..."
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 650781539051.dkr.ecr.us-east-2.amazonaws.com
 
 echo "Stopping old container..."
-docker stop my-web-app || true
-docker rm my-web-app || true
+docker stop my-container || true
+docker rm my-container || true
 
 echo "Pulling new image..."
 docker pull $IMAGE
 
 echo "Running container..."
-docker run -d --name my-web-app -p 80:80 $IMAGE
+docker run -d --name my-container -p 80:80 $IMAGE
